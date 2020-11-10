d3c74aae6db795d6f197ec9cc123a0b9048982d0
Natalia Danilova <nd858768@gmail.com>
2020-11-10 20:49:38 +0300

    hello

--- marvelbase/comics/urls.py
+++ marvelbase/comics/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('save/<comic>', views.save_comic, name='save_comic'),
+    path('show-results/<title>', views.show_results, name='show_results'),
+    #path('save/<comic>', views.save_comic, name='save_comic'),
 ]
