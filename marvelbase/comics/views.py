d9a0f87d1d211b408b73a6fef3df0e4a394593f1
Natalia Danilova <nd858768@gmail.com>
2021-01-10 22:36:08 +0300

    без цикла

--- marvelbase/comics/views.py
+++ marvelbase/comics/views.py
@@ -57,20 +57,18 @@ def show_results(request, title):
 
 def comic_info(request, id):
         all_comics = json.load(open('comic-information.json'))
-        for one_comic in all_comics:
-            if one_comic['id'] >= 0 & one_comic['id'] < len(all_comics):  # здесь что-то не то...
-                with open('one-comic-info.json', 'w') as file2:
-                    json.dump(all_comics[id], file2, indent=2)
-                if request.method == "POST":
-                    save_comic = json.load(open('one-comic-info.json'))
-                    dbcomic = ComicData(request.POST)
-                    #(title=save_comic['title'],description=save_comic['description'], thumbnail=save_comic['thumbnail'], variants=save_comic['variants'], ean=save_comic['ean'], dates=save_comic['dates'])
-                    dbcomic.title = save_comic['title']
-                    dbcomic.description = save_comic['description']
-                    dbcomic.thumbnail = save_comic['thumbnail']
-                    dbcomic.variants = save_comic['variants']
-                    dbcomic.ean = save_comic['ean']
-                    dbcomic.dates = save_comic['dates']
-                    dbcomic.save()
-
-            return render(request, 'comic-info.html', {'id': id, 'infocomic': all_comics[id]})
+        if all_comics[id]['id'] >= 0 & all_comics[id]['id'] < len(all_comics):  # здесь что-то не то...
+            with open('one-comic-info.json', 'w') as file2:
+                json.dump(all_comics[id], file2, indent=2)
+            if request.method == "POST":
+                save_comic = json.load(open('one-comic-info.json'))
+                dbcomic = ComicData(request.POST)
+                #(title=save_comic['title'],description=save_comic['description'], thumbnail=save_comic['thumbnail'], variants=save_comic['variants'], ean=save_comic['ean'], dates=save_comic['dates'])
+                dbcomic.title = save_comic['title']
+                dbcomic.description = save_comic['description']
+                dbcomic.thumbnail = save_comic['thumbnail']
+                dbcomic.variants = save_comic['variants']
+                dbcomic.ean = save_comic['ean']
+                dbcomic.dates = save_comic['dates']
+                dbcomic.save()
+        return render(request, 'comic-info.html', {'id': id, 'infocomic': all_comics[id]})
