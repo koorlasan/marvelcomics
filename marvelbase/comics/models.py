58c275bab5d6f7719acf6040db49de8f00879144
Natalia Danilova <nd858768@gmail.com>
2021-01-24 21:43:37 +0300

    загрузка картинок в папку Pictures

--- marvelbase/comics/models.py
+++ marvelbase/comics/models.py
@@ -6,7 +6,7 @@ import json
 class ComicData(models.Model):
     title = models.CharField(max_length=1024, default=None)
     description = models.TextField(null=True, default=None)
-    thumbnail = models.ImageField(null=True, default=None)
+    thumbnail = models.ImageField(null=True, default=None, upload_to='Pictures')
     dates = models.DateField(null=True, default=None)
     #  variants = models.TextField()
     #  ean = models.TextField()
